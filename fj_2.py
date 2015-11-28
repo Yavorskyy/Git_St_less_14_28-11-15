59df2f10dcf27bcc7b2ac6c25939a76e06c02e75
SerhSt <benosergio@gmail.com>
2015-11-28 11:19:44 +0200

    Create fj_2.py

---
+++ fj_2.py
@@ -0,0 +1,9 @@
+sister_age = 55
+broser_age = 55
+if sister_age > broser_age:
+    print("sister is older")
+elif broser_age > sister_age:
+    print("brother is older")
+        
+else:
+    print("they are same age")
